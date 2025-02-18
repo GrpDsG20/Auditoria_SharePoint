abfc8278dc7cc634094d2685da1f1fa6e0c5ce27
Smith <59782720+GrpDsG20@users.noreply.github.com>
2025-02-17 22:32:48 -0500

    Update SharePoint.ps1

--- SharePoint.ps1
+++ SharePoint.ps1
@@ -5,6 +5,42 @@ Update-Module -Name Microsoft.Online.SharePoint.PowerShell
 
 Connect-SPOService https://m365x14389705-admin.sharepoint.com/
 
+
+# Para que este script funcione correctamente, es necesario tener permisos de Administrador de Colección de Sitios y ser Owner (propietario) en todos los sitios de SharePoint Online que se gestionen.
+# ------------- Permisos de Owner en todos los sitios de SharePoint -------------
+
+# Conectar a SharePoint Online
+Connect-SPOService -Url "https://m365x14389705-admin.sharepoint.com/"
+
+# Obtener todos los sitios de SharePoint
+$sites = Get-SPOSite -Limit ALL
+
+# Tu dirección de correo electrónico
+$yourEmail = "tu_correo@ejemplo.com"
+
+foreach ($site in $sites) {
+    # Añadirte como Owner en cada sitio
+    Set-SPOSite -Identity $site.Url -Owner $yourEmail
+    Write-Host "Te has agregado como Owner en: $($site.Url)"
+}
+
+
+# ------------- Administrador de colección de sitios -------------
+
+# Establecer conexión a SharePoint Online
+$AdminCenterURL = "https://tu-tenant-admin.sharepoint.com"
+Connect-SPOService -Url $AdminCenterURL
+
+# Obtener todos los sitios
+$sites = Get-SPOSite -Limit ALL
+
+# Agregar tu cuenta como propietario a cada sitio
+foreach ($site in $sites) {
+    Set-SPOUser -Site $site.Url -LoginName "tu-correo@tu-dominio.com" -IsSiteCollectionAdmin $true
+}
+
+
+
 # Verificar el estado del sitio: Puedes revisar si el sitio está en modo de solo lectura o si está en proceso de eliminación utilizando PowerShell
 Get-SPOSite -Identity $site.Url | Select ReadOnly, LockState
 
